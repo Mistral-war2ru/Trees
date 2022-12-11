e500d7be81705e88677b14b9b4ab803468d6a39d
Mistral-war2ru <82323152+Mistral-war2ru@users.noreply.github.com>
2022-12-11 21:09:06 +0300

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Trees
+{
+    public partial class Form1 : Form
+    {
+        Tree tree;
+        Random rand = new Random();
+        int move_x = 0, move_y = 0, size = 32;
+        bool redraw;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            pictureBox1.Top = 0;
+            pictureBox1.Left = 0;
+            pictureBox1.Width = Width - 16;
+            pictureBox1.Height = Height - pictureBox1.Location.Y - 39;
+            if (redraw)
+            {
+                SolidBrush white = new SolidBrush(Color.White);
+                Graphics g = Graphics.FromHwnd(pictureBox1.Handle);
+                g.FillRectangle(white, 0, 0, pictureBox1.Width, pictureBox1.Height);
+                white.Dispose();
+                tree.recalc();
+                tree.Draw(g, move_x + pictureBox1.Width / 2, 4 + move_y);
+                redraw = false;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            tree = new Tree(rand.Next(-10, 10), 0, size);
+            WindowState = FormWindowState.Maximized;
+        }
+
+        private void Form1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == 'f')
+            {
+                move_x = 0;
+                move_y = 0;
+                tree = new Tree(rand.Next(-10, 10), 0, size);
+                for (int i = 0; i < 100; i++) tree.Add(rand.Next(-100, 100));
+                tree.recalc();
+                move_x = -tree.dx;
+                redraw = true;
+                Invalidate();
+            }
+            if (e.KeyChar == 'q')
+            {
+                move_x = 0;
+                move_y = 0;
+                tree = new Tree(rand.Next(-10, 10), 0, size);
+                redraw = true;
+                Invalidate();
+            }
+            if (e.KeyChar == 'e')
+            {
+                tree.Add(rand.Next(-100, 100));
+                redraw = true;
+                Invalidate();
+            }
+            if (e.KeyChar == 'c')
+            {
+                for (int i = 0; i < 100; i++) tree.Add(rand.Next(-100, 100));
+                redraw = true;
+                Invalidate();
+            }
+            if (e.KeyChar == 'r')
+            {
+                redraw = true;
+                Invalidate();
+            }
+            if (e.KeyChar == 'w')
+            {
+                move_y -= tree.node_size * 5;
+                redraw = true;
+                Invalidate();
+            }
+            if (e.KeyChar == 's')
+            {
+                move_y += tree.node_size * 5;
+                redraw = true;
+                Invalidate();
+            }
+            if (e.KeyChar == 'a')
+            {
+                move_x -= tree.node_size * 5;
+                redraw = true;
+                Invalidate();
+            }
+            if (e.KeyChar == 'd')
+            {
+                move_x += tree.node_size * 5;
+                redraw = true;
+                Invalidate();
+            }
+            if (e.KeyChar == 'z')
+            {
+                if (size > 1) size--;
+                tree.chsz(size);
+                redraw = true;
+                Invalidate();
+            }
+            if (e.KeyChar == 'x')
+            {
+                size++;
+                tree.chsz(size);
+                redraw = true;
+                Invalidate();
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Trees
+{
+    public class Tree
+    {
+        public int node_size = 32;
+
+        public int x;
+        public int dx = 0;
+        public Tree left;
+        public Tree right;
+
+        public Tree(int xx, int dxx,int s)
+        {
+            x = xx;
+            dx = dxx;
+            node_size = s;
+        }
+
+        public void Add(int xx)
+        {
+            if (xx <= x)
+            {
+                if (left == null)
+                {
+                    Tree t = new Tree(xx, dx - node_size, node_size);
+                    left = t;
+                }
+                else left.Add(xx);
+            }
+            else
+            {
+                if (right == null)
+                {
+                    Tree t = new Tree(xx, dx + node_size, node_size);
+                    right = t;
+                }
+                else right.Add(xx);
+            }
+        }
+
+        private int max_dx()
+        {
+            int m = dx;
+            if (left != null)
+            {
+                int mm = left.max_dx();
+                m = mm > m ? mm : m;
+            }
+            if (right != null)
+            {
+                int mm = right.max_dx();
+                m = mm > m ? mm : m;
+            }
+            return m;
+        }
+
+        private int min_dx()
+        {
+            int m = dx;
+            if (left != null)
+            {
+                int mm = left.min_dx();
+                m = mm < m ? mm : m;
+            }
+            if (right != null)
+            {
+                int mm = right.min_dx();
+                m = mm < m ? mm : m;
+            }
+            return m;
+        }
+
+        private void move(int xx)
+        {
+            dx += xx;
+            if (left != null) left.move(xx);
+            if (right != null) right.move(xx);
+        }
+
+        private bool calc()
+        {
+            bool c = false;
+            if (left != null)
+            {
+                if (left.calc()) c = true;
+                int mx = left.max_dx();
+                if (mx >= dx)
+                {
+                    dx += mx - dx + node_size;
+                    c = true;
+                }
+            }
+            if (right != null)
+            {
+                int mn = right.min_dx();
+                if (mn <= dx)
+                {
+                    right.move(dx - mn + node_size);
+                    c = true;
+                }
+                if (right.calc()) c = true;
+            }
+            return c;
+        }
+
+        public void recalc()
+        {
+            while (calc());
+        }
+
+        public void chsz(int s)
+        {
+            node_size = s;
+            if (left != null) left.chsz(s);
+            if (right != null) right.chsz(s);
+        }
+
+        public void Draw(Graphics g, int x, int y)
+        {
+            SolidBrush white = new SolidBrush(Color.White);
+            SolidBrush black_text = new SolidBrush(Color.Black);
+            Pen black = new Pen(Color.Black);
+            Font drawFont = new Font("Arial", 8);
+            StringFormat drawFormat = new StringFormat();
+            int yy = y + node_size * 2;
+            if (left != null)
+            {
+                g.DrawLine(black, x + dx, y + node_size / 2, x + left.dx, yy + node_size / 2);
+                left.Draw(g, x, yy);
+            }
+            if (right != null)
+            {
+                g.DrawLine(black, x + dx, y + node_size / 2, x + right.dx, yy + node_size / 2);
+                right.Draw(g, x, yy);
+            }
+            g.FillEllipse(white, x + dx - node_size / 2, y - node_size / 32, node_size, node_size);
+            g.DrawEllipse(black, x + dx - node_size / 2, y - node_size / 32, node_size, node_size);
+            g.DrawString(this.x.ToString(), drawFont, black_text, x + dx - node_size / 4, y + node_size / 4, drawFormat);
+
+            black.Dispose();
+            white.Dispose();
+            black_text.Dispose();
+            drawFont.Dispose();
+            drawFormat.Dispose();
+        }
+    }
+
+    static class Program
+    {
+        /// <summary>
+        /// Главная точка входа для приложения.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
